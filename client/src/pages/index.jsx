e1972dcd0a2df89b768c9cdb79d765d94d5d3b62
untitlez <tleza_championship2@hotmail.com>
2026-01-06 12:55:12 +0700

    add : cookie banner

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Card, Layout, Progress } from "antd";
+import { Card, Layout, Progress, Alert, Button } from "antd";
 import { Content } from "antd/es/layout/layout";
 import axios from "axios";
 
@@ -7,20 +7,29 @@ import { Config } from "../lib/config";
 import { routes } from "../lib/routes";
 import { LoginForm } from "./auth";
 import { ConfigTheme } from "../components/ConfigTheme";
-import { Background } from "../components/Background";
 
 export default function HomePage() {
   const [ready, setReady] = useState(false);
   const [progress, setProgress] = useState(30);
+  const [show, setShow] = useState(false);
 
   const wakeUpServer = async () => {
     await axios.get(Config.API_URL + routes.users);
     setProgress(99);
   };
 
+  const acceptCookies = () => {
+    localStorage.setItem("cookie_consent", "accepted");
+    setShow(false);
+  };
+
+  const consent = localStorage.getItem("cookie_consent");
+
   useEffect(() => {
     wakeUpServer();
     setProgress(60);
+
+    if (!consent) setShow(true);
   }, []);
 
   useEffect(() => {
@@ -29,8 +38,23 @@ export default function HomePage() {
 
   return (
     <ConfigTheme>
-      <Layout className="h-screen relative">
-        <Background />
+      <Layout className="h-screen">
+        {/* Cookie  */}
+        {show && (
+          <Alert
+            title="We Value Your Privacy"
+            showIcon
+            description="These cookies are required for core website functionality such as authentication and security. They cannot be disabled."
+            type="warning"
+            action={
+              <Button size="small" type="primary" onClick={acceptCookies}>
+                Accept
+              </Button>
+            }
+          />
+        )}
+
+        {/* Form  */}
         {ready ? (
           <Content width="100%" className="grid place-content-center">
             <Card title="Sign In" className="w-full max-w-sm">
